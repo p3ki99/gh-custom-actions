3fd1496766a7a032fbeb4e91232d682e7e1d8ef0
Petar Petrovic <p3ki99@gmail.com>
2024-03-10 14:54:38 +0100

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,18 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 async function run() {
-  core.notice("Hello from my custom JavaScript Action!");
+  // 1) Get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`
+  AWS_ACCESS_KEY_ID="bucket-access-key"
+  exec.exec("")
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  
+  core.notice('Hello from my custom JavaScript Action!');
 }
 
 run();
